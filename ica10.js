50e99823d964bb1b974a22ee4423ea6ea5d13937
caseyhunt <casey.l.hunt@gmail.com>
2024-11-12 10:19:46 -0700

    Revert "wa11"
    This reverts commit 9780e7eba7ca2144be93874f31437555b948c357.

---
+++ ica10.js
@@ -0,0 +1,6 @@
+document.getElementById("cammy").addEventListener("click", function() {
+  alert("Button clicked!");
+});
+
+document.getElementById("paragraph").style.color = "pink";
+document.getElementById("head").style.color = "red";
